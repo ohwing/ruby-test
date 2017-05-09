b374c8122d44847261ab89540cf65fce039658a2
EC2 Default User <ec2-user@ip-172-31-5-25.ap-northeast-2.compute.internal>
2017-05-09 13:21:40 +0000

    del

--- db/migrate/20170509083408_create_questions.rb
+++
@@ -1,9 +0,0 @@
-class CreateQuestions < ActiveRecord::Migration[5.0]
-  def change
-    create_table :questions do |t|
-      t.string :name
-      t.string :content
-      t.timestamps null: false
-    end
-  end
-end
