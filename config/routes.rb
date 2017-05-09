99102faaccf26d24112cb0cf90675445ba98d5f2
EC2 Default User <ec2-user@ip-172-31-5-25.ap-northeast-2.compute.internal>
2017-05-09 08:37:08 +0000

    controller add

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
+
+  get 'home/index'
   get 'home/write'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
