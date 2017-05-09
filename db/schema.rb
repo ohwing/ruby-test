161ce2c017ad9d244e949b800aa4272a28030ad5
EC2 Default User <ec2-user@ip-172-31-5-25.ap-northeast-2.compute.internal>
2017-05-09 12:48:14 +0000

    으아아앙ㄴ돼

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,8 @@
 ActiveRecord::Schema.define(version: 20170509083408) do
 
   create_table "questions", force: :cascade do |t|
+    t.string   "name"
+    t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
