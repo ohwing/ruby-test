bbe4bff6213fda6da78e63a36f62a44467368314
ohjeung <ohjeung@likelion.org>
2017-05-09 18:00:41 +0900

    레일즈 db get 추가

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
-
-  get 'home/index'
-  get 'home/write'
+  get 'rails/db'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
